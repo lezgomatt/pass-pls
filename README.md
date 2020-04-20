f3c32ea7bffd9d8ca4eab4a58a190ac62574c25b
Matt <undecidabot@gmail.com>
2020-04-20 21:20:32 +0800

    docs: Readme update and screenshots

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # pass-pls
 
-A simple ssh-askpass implementation using GTK.
+A GTK-based implemention of ssh-askpass.
+
+**Why?** Because `ssh-askpass-gnome` was asking for a password even for confirmations.
+Also, its lack of margins bothered me.
+
+## Screenshots
+
+![Ask](https://raw.githubusercontent.com/undecidabot/pass-pls/master/screenshots/ask.png)
+
+![Confirm](https://raw.githubusercontent.com/undecidabot/pass-pls/master/screenshots/confirm.png)
+
+## Installation
+
+To install:
+```
+sudo pip3 install pass-pls
+sudo update-alternatives --install /usr/bin/ssh-askpass ssh-askpass /usr/local/bin/pass-pls 100
+```
+
+To uninstall:
+```
+sudo update-alternatives --remove ssh-askpass /usr/local/bin/pass-pls
+```
+
+If you do not have `update-alternatives`, manually create a symlink instead.
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as readme:
 setuptools.setup(
     name="pass-pls",
     version="0.0.1",
-    description="A simple ssh-askpass implementation using GTK",
+    description="A GTK-based implemention of ssh-askpass",
     url="https://github.com/undecidabot/pass-pls",
     author="Matt",
     author_email="undecidabot@gmail.com",
@@ -14,16 +14,15 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     classifiers=[
+        "Development Status :: 5 - Production/Stable",
         "Environment :: X11 Applications :: GTK",
         "Intended Audience :: Developers",
         "Intended Audience :: System Administrators",
         "License :: OSI Approved :: zlib/libpng License",
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Topic :: Security",
         "Topic :: System :: Networking",
         "Topic :: System :: Systems Administration",
@@ -35,6 +34,6 @@ setuptools.setup(
         "PyGObject",
     ],
     entry_points = {
-        'console_scripts': ['pass-pls=pass_pls:main'],
+        "console_scripts": ["pass-pls=pass_pls:main"],
     },
 )
